9fd929a6a445b412a503edb07e20f2046a457fcd
remiposo <57336808+remiposo@users.noreply.github.com>
2025-08-22 15:44:38 +0900

    Merge pull request #308 from tailor-platform/remiposo/2025-08-21-support-permission
    feat: support Permission/GQLPermission with Operator API

--- packages/tailor-sdk/src/apply/services/tailordb.ts
+++ packages/tailor-sdk/src/apply/services/tailordb.ts
@@ -1,22 +1,47 @@
-import { MessageInitShape } from "@bufbuild/protobuf";
+import { fromJson, MessageInitShape } from "@bufbuild/protobuf";
+import { ValueSchema } from "@bufbuild/protobuf/wkt";
 import { Code, ConnectError } from "@connectrpc/connect";
 
 import {
+  CreateTailorDBGQLPermissionRequestSchema,
   CreateTailorDBServiceRequestSchema,
   CreateTailorDBTypeRequestSchema,
+  DeleteTailorDBGQLPermissionRequestSchema,
   DeleteTailorDBServiceRequestSchema,
   DeleteTailorDBTypeRequestSchema,
+  UpdateTailorDBGQLPermissionRequestSchema,
   UpdateTailorDBTypeRequestSchema,
 } from "@/gen/tailor/v1/tailordb_pb";
 import {
+  TailorDBGQLPermission_Action,
+  TailorDBGQLPermission_ConditionSchema,
+  TailorDBGQLPermission_OperandSchema,
+  TailorDBGQLPermission_Operator,
+  TailorDBGQLPermission_Permit,
+  TailorDBGQLPermission_PolicySchema,
+  TailorDBGQLPermissionSchema,
   TailorDBType_FieldConfigSchema,
   TailorDBType_IndexSchema,
+  TailorDBType_Permission_ConditionSchema,
+  TailorDBType_Permission_OperandSchema,
+  TailorDBType_Permission_Operator,
+  TailorDBType_Permission_Permit,
+  TailorDBType_Permission_PolicySchema,
+  TailorDBType_PermissionSchema,
   TailorDBType_PermitAction,
   TailorDBType_RelationshipConfigSchema,
   TailorDBTypeSchema,
 } from "@/gen/tailor/v1/tailordb_resource_pb";
 import { ApplyOptions } from "@/generator/options";
-import { ExecutorService } from "@/services";
+import { Executor } from "@/services";
+import {
+  PermissionOperand,
+  StandardActionPermission,
+  StandardGqlPermissionPolicy,
+  StandardPermissionCondition,
+  StandardTailorTypeGqlPermission,
+  StandardTailorTypePermission,
+} from "@/services/tailordb/permission";
 import { TailorDBType } from "@/services/tailordb/schema";
 import { TailorDBService } from "@/services/tailordb/service";
 import { DBFieldMetadata } from "@/services/tailordb/types";
@@ -36,14 +61,11 @@ export async function applyTailorDB(
     return;
   }
 
+  // Create / Update
   // Services
   for (const create of changeSet.service.creates) {
     await client.createTailorDBService(create.request);
   }
-  for (const del of changeSet.service.deletes) {
-    await client.deleteTailorDBService(del.request);
-  }
-
   // Types
   for (const create of changeSet.type.creates) {
     await client.createTailorDBType(create.request);
@@ -51,10 +73,30 @@ export async function applyTailorDB(
   for (const update of changeSet.type.updates) {
     await client.updateTailorDBType(update.request);
   }
+  // GQLPermissions
+  for (const create of changeSet.gqlPermission.creates) {
+    await client.createTailorDBGQLPermission(create.request);
+  }
+  for (const update of changeSet.gqlPermission.updates) {
+    await client.updateTailorDBGQLPermission(update.request);
+  }
+
+  // Delete (Execute in reverse order considering dependencies)
+  // GQLPermissions
+  for (const del of changeSet.gqlPermission.deletes) {
+    if (del.tag === "gql-permission-deleted") {
+      await client.deleteTailorDBGQLPermission(del.request);
+    }
+  }
+  // Types
   for (const del of changeSet.type.deletes) {
     if (del.tag === "type-deleted") {
       await client.deleteTailorDBType(del.request);
     }
+  }
+  // Services
+  for (const del of changeSet.service.deletes) {
+    await client.deleteTailorDBService(del.request);
   }
 }
 
@@ -66,24 +108,37 @@ async function planTailorDB(
   const tailordbs: TailorDBService[] = [];
   for (const app of workspace.applications) {
     for (const tailordb of app.tailorDBServices) {
+      await tailordb.loadTypes();
       tailordbs.push(tailordb);
     }
   }
+  const executors = Object.values(
+    (await workspace.executorService?.loadExecutors()) ?? {},
+  );
+
   const serviceChangeSet = await planServices(client, workspaceId, tailordbs);
   const deletedServices = serviceChangeSet.deletes.map((del) => del.name);
   const typeChangeSet = await planTypes(
     client,
     workspaceId,
     tailordbs,
-    workspace.executorService,
+    executors,
+    deletedServices,
+  );
+  const gqlPermissionChangeSet = await planGqlPermissions(
+    client,
+    workspaceId,
+    tailordbs,
     deletedServices,
   );
 
   serviceChangeSet.print();
   typeChangeSet.print();
+  gqlPermissionChangeSet.print();
   return {
     service: serviceChangeSet,
     type: typeChangeSet,
+    gqlPermission: gqlPermissionChangeSet,
   };
 }
 
@@ -95,6 +150,11 @@ type CreateService = {
 type DeleteService = {
   name: string;
   request: MessageInitShape<typeof DeleteTailorDBServiceRequestSchema>;
+};
+
+type ServiceDeleted = {
+  tag: "service-deleted";
+  name: string;
 };
 
 async function planServices(
@@ -173,16 +233,11 @@ type DeleteType = {
   request: MessageInitShape<typeof DeleteTailorDBTypeRequestSchema>;
 };
 
-type ServiceDeleted = {
-  tag: "service-deleted";
-  name: string;
-};
-
 async function planTypes(
   client: OperatorClient,
   workspaceId: string,
   tailordbs: ReadonlyArray<TailorDBService>,
-  executor: Readonly<ExecutorService> | undefined,
+  executors: ReadonlyArray<Executor>,
   deletedServices: ReadonlyArray<string>,
 ) {
   const changeSet: ChangeSet<
@@ -191,7 +246,7 @@ async function planTypes(
     DeleteType | ServiceDeleted
   > = new ChangeSet("TailorDB types");
 
-  const fetchClients = (namespaceName: string) => {
+  const fetchTypes = (namespaceName: string) => {
     return fetchAll(async (pageToken) => {
       try {
         const { tailordbTypes, nextPageToken } = await client.listTailorDBTypes(
@@ -212,7 +267,6 @@ async function planTypes(
   };
 
   const executorUsedTypes = new Set<string>();
-  const executors = Object.values((await executor?.loadExecutors()) ?? {});
   for (const executor of executors) {
     const triggerContext = executor.trigger.context;
     if ("type" in triggerContext && triggerContext.type) {
@@ -221,12 +275,11 @@ async function planTypes(
   }
 
   for (const tailordb of tailordbs) {
-    const existingTypes = await fetchClients(tailordb.namespace);
+    const existingTypes = await fetchTypes(tailordb.namespace);
     const existingNameSet = new Set<string>();
     existingTypes.forEach((typ) => {
       existingNameSet.add(typ.name);
     });
-    await tailordb.loadTypes();
     for (const fileTypes of Object.values(tailordb.getTypes())) {
       for (const typ of Object.values(fileTypes)) {
         const tailordbType = generateTailorDBTypeManifest(
@@ -268,11 +321,11 @@ async function planTypes(
     });
   }
   for (const namespaceName of deletedServices) {
-    const existingTypes = await fetchClients(namespaceName);
-    existingTypes.forEach((client) => {
+    const existingTypes = await fetchTypes(namespaceName);
+    existingTypes.forEach((typ) => {
       changeSet.deletes.push({
         tag: "service-deleted",
-        name: client.name,
+        name: typ.name,
       });
     });
   }
@@ -287,14 +340,6 @@ function generateTailorDBTypeManifest(
 ): MessageInitShape<typeof TailorDBTypeSchema> {
   const metadata = type.metadata;
   const schema = metadata.schema;
-
-  const defaultTypePermission = {
-    create: [{ id: "everyone", permit: TailorDBType_PermitAction.ALLOW }],
-    read: [{ id: "everyone", permit: TailorDBType_PermitAction.ALLOW }],
-    update: [{ id: "everyone", permit: TailorDBType_PermitAction.ALLOW }],
-    delete: [{ id: "everyone", permit: TailorDBType_PermitAction.ALLOW }],
-    admin: [{ id: "everyone", permit: TailorDBType_PermitAction.ALLOW }],
-  };
 
   const defaultSettings = {
     aggregation: schema?.settings?.aggregation || false,
@@ -440,6 +485,20 @@ function generateTailorDBTypeManifest(
     });
   }
 
+  // To be secure by default, add Permission settings that reject everyone
+  // when Permission/RecordPermission is not configured.
+  const defaultPermission: MessageInitShape<
+    typeof TailorDBType_PermissionSchema
+  > = {
+    create: [],
+    read: [],
+    update: [],
+    delete: [],
+  };
+  const permission = schema.permissions.record
+    ? protoPermission(schema.permissions.record)
+    : defaultPermission;
+
   return {
     name: metadata.name || type.name,
     schema: {
@@ -450,7 +509,7 @@ function generateTailorDBTypeManifest(
       extends: schema?.extends || false,
       directives: [],
       indexes: indexes,
-      typePermission: defaultTypePermission,
+      permission,
     },
   };
 }
@@ -515,4 +574,343 @@ function processNestedFields(
   );
 
   return nestedFields;
+}
+
+function protoPermission(
+  permission: StandardTailorTypePermission,
+): MessageInitShape<typeof TailorDBType_PermissionSchema> {
+  const ret: MessageInitShape<typeof TailorDBType_PermissionSchema> = {};
+  for (const [key, policies] of Object.entries(permission)) {
+    ret[key as keyof StandardTailorTypePermission] = policies.map((policy) =>
+      protoPolicy(policy),
+    );
+  }
+  return ret;
+}
+
+function protoPolicy(
+  policy: StandardActionPermission<"record">,
+): MessageInitShape<typeof TailorDBType_Permission_PolicySchema> {
+  let permit: TailorDBType_Permission_Permit;
+  switch (policy.permit) {
+    case "allow":
+      permit = TailorDBType_Permission_Permit.ALLOW;
+      break;
+    case "deny":
+      permit = TailorDBType_Permission_Permit.DENY;
+      break;
+    default:
+      throw new Error(`Unknown permission: ${policy.permit satisfies never}`);
+  }
+  return {
+    conditions: policy.conditions.map((cond) => protoCondition(cond)),
+    permit,
+    description: policy.description,
+  };
+}
+
+function protoCondition(
+  condition: StandardPermissionCondition<"record">,
+): MessageInitShape<typeof TailorDBType_Permission_ConditionSchema> {
+  const [left, operator, right] = condition;
+
+  const l = protoOperand(left);
+  const r = protoOperand(right);
+  let op: TailorDBType_Permission_Operator;
+  switch (operator) {
+    case "eq":
+      op = TailorDBType_Permission_Operator.EQ;
+      break;
+    case "ne":
+      op = TailorDBType_Permission_Operator.NE;
+      break;
+    case "in":
+      op = TailorDBType_Permission_Operator.IN;
+      break;
+    case "nin":
+      op = TailorDBType_Permission_Operator.NIN;
+      break;
+    default:
+      throw new Error(`Unknown operator: ${operator satisfies never}`);
+  }
+  return {
+    left: l,
+    operator: op,
+    right: r,
+  };
+}
+
+function protoOperand(
+  operand: PermissionOperand<
+    "record",
+    Record<string, unknown>,
+    Record<string, unknown>
+  >,
+): MessageInitShape<typeof TailorDBType_Permission_OperandSchema> {
+  if (typeof operand === "object" && !Array.isArray(operand)) {
+    if ("user" in operand) {
+      return {
+        kind: {
+          case: "userField",
+          value: operand.user,
+        },
+      };
+    } else if ("record" in operand) {
+      return {
+        kind: {
+          case: "recordField",
+          value: operand.record,
+        },
+      };
+    } else if ("newRecord" in operand) {
+      return {
+        kind: {
+          case: "newRecordField",
+          value: operand.newRecord,
+        },
+      };
+    } else if ("oldRecord" in operand) {
+      return {
+        kind: {
+          case: "oldRecordField",
+          value: operand.oldRecord,
+        },
+      };
+    } else {
+      throw new Error(`Unknown operand: ${operand satisfies never}`);
+    }
+  }
+
+  return {
+    kind: {
+      case: "value",
+      value: fromJson(ValueSchema, operand),
+    },
+  };
+}
+
+type CreateGqlPermission = {
+  name: string;
+  request: MessageInitShape<typeof CreateTailorDBGQLPermissionRequestSchema>;
+};
+
+type UpdateGqlPermission = {
+  name: string;
+  request: MessageInitShape<typeof UpdateTailorDBGQLPermissionRequestSchema>;
+};
+
+type DeleteGqlPermission = {
+  tag: "gql-permission-deleted";
+  name: string;
+  request: MessageInitShape<typeof DeleteTailorDBGQLPermissionRequestSchema>;
+};
+
+async function planGqlPermissions(
+  client: OperatorClient,
+  workspaceId: string,
+  tailordbs: ReadonlyArray<TailorDBService>,
+  deletedServices: ReadonlyArray<string>,
+) {
+  const changeSet: ChangeSet<
+    CreateGqlPermission,
+    UpdateGqlPermission,
+    DeleteGqlPermission | ServiceDeleted
+  > = new ChangeSet("TailorDB gqlPermissions");
+
+  const fetchGqlPermissions = (namespaceName: string) => {
+    return fetchAll(async (pageToken) => {
+      try {
+        const { permissions, nextPageToken } =
+          await client.listTailorDBGQLPermissions({
+            workspaceId,
+            namespaceName,
+            pageToken,
+          });
+        return [permissions, nextPageToken];
+      } catch (error) {
+        if (error instanceof ConnectError && error.code === Code.NotFound) {
+          return [[], ""];
+        }
+        throw error;
+      }
+    });
+  };
+
+  for (const tailordb of tailordbs) {
+    const existingGqlPermissions = await fetchGqlPermissions(
+      tailordb.namespace,
+    );
+    const existingNameSet = new Set<string>();
+    existingGqlPermissions.forEach((gqlPermission) => {
+      existingNameSet.add(gqlPermission.typeName);
+    });
+    for (const fileTypes of Object.values(tailordb.getTypes())) {
+      for (const typ of Object.values(fileTypes)) {
+        const gqlPermission = typ.metadata.schema.permissions.gql;
+        if (!gqlPermission) {
+          continue;
+        }
+        if (existingNameSet.has(typ.name)) {
+          changeSet.updates.push({
+            name: typ.name,
+            request: {
+              workspaceId,
+              namespaceName: tailordb.namespace,
+              typeName: typ.name,
+              permission: protoGqlPermission(gqlPermission),
+            },
+          });
+          existingNameSet.delete(typ.name);
+        } else {
+          changeSet.creates.push({
+            name: typ.name,
+            request: {
+              workspaceId,
+              namespaceName: tailordb.namespace,
+              typeName: typ.name,
+              permission: protoGqlPermission(gqlPermission),
+            },
+          });
+        }
+      }
+    }
+    existingNameSet.forEach((name) => {
+      changeSet.deletes.push({
+        tag: "gql-permission-deleted",
+        name,
+        request: {
+          workspaceId,
+          namespaceName: tailordb.namespace,
+          typeName: name,
+        },
+      });
+    });
+  }
+  for (const namespaceName of deletedServices) {
+    const existingGqlPermissions = await fetchGqlPermissions(namespaceName);
+    existingGqlPermissions.forEach((gqlPermission) => {
+      changeSet.deletes.push({
+        tag: "service-deleted",
+        name: gqlPermission.typeName,
+      });
+    });
+  }
+  return changeSet;
+}
+
+function protoGqlPermission(
+  permission: StandardTailorTypeGqlPermission,
+): MessageInitShape<typeof TailorDBGQLPermissionSchema> {
+  return {
+    policies: permission.map((policy) => protoGqlPolicy(policy)),
+  };
+}
+
+function protoGqlPolicy(
+  policy: StandardGqlPermissionPolicy,
+): MessageInitShape<typeof TailorDBGQLPermission_PolicySchema> {
+  const actions: TailorDBGQLPermission_Action[] = [];
+  for (const action of policy.actions) {
+    switch (action) {
+      case "all":
+        actions.push(TailorDBGQLPermission_Action.ALL);
+        break;
+      case "create":
+        actions.push(TailorDBGQLPermission_Action.CREATE);
+        break;
+      case "read":
+        actions.push(TailorDBGQLPermission_Action.READ);
+        break;
+      case "update":
+        actions.push(TailorDBGQLPermission_Action.UPDATE);
+        break;
+      case "delete":
+        actions.push(TailorDBGQLPermission_Action.DELETE);
+        break;
+      case "aggregate":
+        actions.push(TailorDBGQLPermission_Action.AGGREGATE);
+        break;
+      case "bulkUpsert":
+        actions.push(TailorDBGQLPermission_Action.BULK_UPSERT);
+        break;
+      default:
+        throw new Error(`Unknown action: ${action satisfies never}`);
+    }
+  }
+  let permit: TailorDBGQLPermission_Permit;
+  switch (policy.permit) {
+    case "allow":
+      permit = TailorDBGQLPermission_Permit.ALLOW;
+      break;
+    case "deny":
+      permit = TailorDBGQLPermission_Permit.DENY;
+      break;
+    default:
+      throw new Error(`Unknown permission: ${policy.permit satisfies never}`);
+  }
+  return {
+    conditions: policy.conditions.map((cond) => protoGqlCondition(cond)),
+    actions,
+    permit,
+    description: policy.description,
+  };
+}
+
+function protoGqlCondition(
+  condition: StandardPermissionCondition<"gql">,
+): MessageInitShape<typeof TailorDBGQLPermission_ConditionSchema> {
+  const [left, operator, right] = condition;
+
+  const l = protoGqlOperand(left);
+  const r = protoGqlOperand(right);
+  let op: TailorDBGQLPermission_Operator;
+  switch (operator) {
+    case "eq":
+      op = TailorDBGQLPermission_Operator.EQ;
+      break;
+    case "ne":
+      op = TailorDBGQLPermission_Operator.NE;
+      break;
+    case "in":
+      op = TailorDBGQLPermission_Operator.IN;
+      break;
+    case "nin":
+      op = TailorDBGQLPermission_Operator.NIN;
+      break;
+    default:
+      throw new Error(`Unknown operator: ${operator satisfies never}`);
+  }
+  return {
+    left: l,
+    operator: op,
+    right: r,
+  };
+}
+
+function protoGqlOperand(
+  operand: PermissionOperand<
+    "gql",
+    Record<string, unknown>,
+    Record<string, unknown>
+  >,
+): MessageInitShape<typeof TailorDBGQLPermission_OperandSchema> {
+  if (typeof operand === "object" && !Array.isArray(operand)) {
+    if ("user" in operand) {
+      return {
+        kind: {
+          case: "userField",
+          value: operand.user,
+        },
+      };
+    } else {
+      throw new Error(`Unknown operand: ${operand satisfies never}`);
+    }
+  }
+
+  return {
+    kind: {
+      case: "value",
+      value: fromJson(ValueSchema, operand),
+    },
+  };
 }
