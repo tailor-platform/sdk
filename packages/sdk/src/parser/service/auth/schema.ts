64436f00d936631a239c8229c1c94be4c8230ece
Haruto Tanaka <61040486+haru0017@users.noreply.github.com>
2025-11-17 19:23:35 +0900

    chore: make sp key and cert optional (#11)

--- packages/sdk/src/parser/service/auth/schema.ts
+++ packages/sdk/src/parser/service/auth/schema.ts
@@ -10,8 +10,8 @@ const secretValueSchema = z.object({
 const samlBaseSchema = z.object({
   name: z.string(),
   kind: z.literal("SAML"),
-  spCertBase64: secretValueSchema,
-  spKeyBase64: secretValueSchema,
+  spCertBase64: secretValueSchema.optional(),
+  spKeyBase64: secretValueSchema.optional(),
 });
 
 export const OIDCSchema = z.object({
