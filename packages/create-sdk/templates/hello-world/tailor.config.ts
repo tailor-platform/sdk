31f0dad022d0a5c4b867af818dcce25f6322e269
remiposo <57336808+remiposo@users.noreply.github.com>
2025-11-25 17:20:20 +0900

    chore: revert template changes

--- packages/create-sdk/templates/hello-world/tailor.config.ts
+++ packages/create-sdk/templates/hello-world/tailor.config.ts
@@ -2,13 +2,5 @@ import { defineConfig } from "@tailor-platform/sdk";
 
 export default defineConfig({
   name: "hello-world",
-  db: {
-    tailordb: { external: true },
-  },
-  resolver: {
-    "main-resolver": { files: [`./src/resolvers/**/*.ts`] },
-    "my-resolver": { external: true },
-  },
-  idp: [{ name: "my-idp", external: true }],
-  auth: { name: "my-auth", external: true },
+  resolver: { "main-resolver": { files: [`./src/resolvers/**/*.ts`] } },
 });
