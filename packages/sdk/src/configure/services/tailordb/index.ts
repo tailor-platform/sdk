f28d1046f63a1bb71d3dd04830ddd56d57e691d1
IzumiSy <beetle-noise@gmx.com>
2025-11-26 12:13:17 +0900

    fix: export types that db.type def requires in bundling

--- packages/sdk/src/configure/services/tailordb/index.ts
+++ packages/sdk/src/configure/services/tailordb/index.ts
@@ -1,4 +1,5 @@
-export { db } from "./schema";
+export { db, type TailorDBType, type TailorDBField } from "./schema";
+export type { TailorDBInstance } from "./schema";
 export type {
   TailorTypePermission,
   TailorTypeGqlPermission,
