3ba08046c2b138600ceb13e71654b4a40b4b7700
Riku Kurosawa <riku.suny@gmail.com>
2025-12-12 09:39:11 +0900

    chore: add comment to humanize-duration type

--- packages/sdk/src/types/humanize-duration.d.ts
+++ packages/sdk/src/types/humanize-duration.d.ts
@@ -1,3 +1,7 @@
+// humanize-duration ships CommonJS-style typings (`export = humanizeDuration`).
+// Our tsconfig does not enable `esModuleInterop`, but we use a default import.
+// This module augmentation exposes the existing humanizeDuration export as a default
+// so that `import humanizeDuration from "humanize-duration"` type-checks correctly.
 declare module "humanize-duration" {
   export default humanizeDuration;
 }
