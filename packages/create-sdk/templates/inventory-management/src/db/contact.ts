51edbf1a8df744f0a7dfa76fd0deeea10e11fecf
Riku Kurosawa <riku.suny@gmail.com>
2025-12-25 18:39:01 +0900

    chore: format

--- packages/create-sdk/templates/inventory-management/src/db/contact.ts
+++ packages/create-sdk/templates/inventory-management/src/db/contact.ts
@@ -5,12 +5,8 @@ export const contact = db
   .type("Contact", {
     name: db.string().description("Name of the contact"),
     email: db.string().unique().description("Email address of the contact"),
-    phone: db
-      .string({ optional: true })
-      .description("Phone number of the contact"),
-    address: db
-      .string({ optional: true })
-      .description("Address of the contact"),
+    phone: db.string({ optional: true }).description("Phone number of the contact"),
+    address: db.string({ optional: true }).description("Address of the contact"),
     ...db.fields.timestamps(),
   })
   .permission(permissionManager)
