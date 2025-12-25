98d5d6b6ea9047cbb0c56ea394526a710cbb5a3a
Riku Kurosawa <riku.suny@gmail.com>
2025-12-25 17:56:24 +0900

    fix: cycle import

---
+++ packages/sdk/src/cli/bundler/inline-sourcemap.ts
@@ -0,0 +1,4 @@
+// NOTE(haru): Enable inline sourcemaps to preserve original source locations in error stack traces for bundled functions
+// This flag will become unnecessary once function registry is implemented, which will resolve script size issues
+export const enableInlineSourcemap: boolean =
+  process.env.TAILOR_ENABLE_INLINE_SOURCEMAP === "true";
