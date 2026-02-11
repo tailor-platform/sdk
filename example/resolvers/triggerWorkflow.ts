d8e3c6686f3c5003ed4abb834287bde5aa48401f
Akira HIGUCHI <akira_higuchi@tailor.tech>
2026-02-11 21:15:12 +0900

    feat: make definePlugins tree-shakable

--- example/resolvers/triggerWorkflow.ts
+++ example/resolvers/triggerWorkflow.ts
@@ -1,5 +1,5 @@
 import { createResolver, t } from "@tailor-platform/sdk";
-import { auth } from "../tailor.config";
+import { auth } from "../auth";
 import orderProcessingWorkflow from "../workflows/order-processing";
 
 export default createResolver({
