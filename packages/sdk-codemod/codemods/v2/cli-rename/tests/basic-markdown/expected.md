d3733b4d6da46ece4cbe489446aa202711c0fa73
dqn <dqn270@gmail.com>
2026-06-16 09:03:08 +0900

    fix(codemod): bound CLI option rewrites

--- packages/sdk-codemod/codemods/v2/cli-rename/tests/basic-markdown/expected.md
+++ packages/sdk-codemod/codemods/v2/cli-rename/tests/basic-markdown/expected.md
@@ -4,4 +4,6 @@ Use `tailor-sdk login --machine-user` before running `tailor-sdk query --machine
 
 Use `tailor-sdk --json crashreport list` but leave `other-cli --machineuser=ci` alone.
 
+Use `pnpm exec tailor-sdk login --machine-user` but leave `other-cli --machineuser=ci` alone.
+
 Do not rewrite unrelated commands such as `other-cli --machineuser=ci`.
--- packages/sdk-codemod/codemods/v2/cli-rename/tests/basic-shell/expected.sh
+++ packages/sdk-codemod/codemods/v2/cli-rename/tests/basic-shell/expected.sh
@@ -5,6 +5,8 @@ pnpm exec tailor-sdk crashreport list
 pnpm exec tailor-sdk crashreport send --file ./latest.crash.log
 pnpm exec tailor-sdk workflow start approval --machine-user ci
 tailor-sdk query --query 'select 1' --machine-user ci
+tailor-sdk query --query 'select 1;' --machine-user ci
+tailor-sdk query --query "select 1 | 2" --machine-user ci
 tailor-sdk workflow start approval --arg '{"ok":true}' \
   --machine-user ci
 tailor-sdk --json crashreport list
