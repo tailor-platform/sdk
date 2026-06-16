230c096c207090d36e6ea4244aece7a1dcad6c1f
dqn <dqn270@gmail.com>
2026-06-16 21:40:05 +0900

    chore(codemod): move principal migration to follow-up

--- packages/sdk-codemod/codemods/v2/principal-unify/tests/shadowed-sdk-field/input.ts
+++
@@ -1,7 +0,0 @@
-import { db } from "@tailor-platform/sdk";
-
-export function buildField(db: { string(): { hooks(config: unknown): unknown } }) {
-  return db.string().hooks({
-    create: ({ user }: { user: { id: string } }) => user.id,
-  });
-}
