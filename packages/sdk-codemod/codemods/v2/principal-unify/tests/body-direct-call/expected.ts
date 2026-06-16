b972bc2ff882c3eae75b1831c6380ed5820eb9af
dqn <dqn270@gmail.com>
2026-06-16 18:36:10 +0900

    fix(codemod): narrow direct body migration

--- packages/sdk-codemod/codemods/v2/principal-unify/tests/body-direct-call/expected.ts
+++ packages/sdk-codemod/codemods/v2/principal-unify/tests/body-direct-call/expected.ts
@@ -25,3 +25,15 @@ export async function runAsCustomUser() {
     env: {},
   });
 }
+
+export async function runWithFactory() {
+  return await resolver.body({
+    input: { id: "user-2" },
+    ...((user) => ({ caller: user, invoker: user }))(makeUser()),
+    env: {},
+  });
+}
+
+function makeUser() {
+  return customUser;
+}
--- packages/sdk-codemod/codemods/v2/principal-unify/tests/body-direct-call/input.ts
+++ packages/sdk-codemod/codemods/v2/principal-unify/tests/body-direct-call/input.ts
@@ -24,3 +24,15 @@ export async function runAsCustomUser() {
     env: {},
   });
 }
+
+export async function runWithFactory() {
+  return await resolver.body({
+    input: { id: "user-2" },
+    user: makeUser(),
+    env: {},
+  });
+}
+
+function makeUser() {
+  return customUser;
+}
